d12e4a6b5f0f34dd2fc8d631ad2c6472dfe25a15
Elmouinysaleh <36488900+Elmouinysaleh@users.noreply.github.com>
2023-07-23 23:14:13 +0100

    Update functions.c
    handle check 48 and 55 error

--- functions.c
+++ functions.c
@@ -77,50 +77,51 @@ int print_string(va_list types, char buffer[],
 } **/
 
 
+
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size)
 {
     int length = 0, i;
     char *str = va_arg(types, char *);
 
-    UNUSED(buffer);
     UNUSED(flags);
     UNUSED(size);
-    
+
     if (str == NULL)
     {
         str = "(null)";
-        if (precision >= 6)
-            str = "      ";
+        if (precision >= 6 || precision < 0)
+            length = 6;
+        else
+            length = precision;
     }
+    else
+    {
+        while (str[length] != '\0')
+            length++;
 
-    while (str[length] != '\0')
-        length++;
-
-    // Limit the number of characters to be printed based on the precision value
-    if (precision >= 0 && precision < length)
-        length = precision;
+        if (precision >= 0 && precision < length)
+            length = precision;
+    }
 
     if (width > length)
     {
         if (flags & F_MINUS)
         {
-            write(1, &str[0], length);
-            for (i = width - length; i > 0; i--)
+            write(1, str, length);
+            for (i = length; i < width; i++)
                 write(1, " ", 1);
-            return width;
         }
         else
         {
-            for (i = width - length; i > 0; i--)
+            for (i = length; i < width; i++)
                 write(1, " ", 1);
-            write(1, &str[0], length);
-            return width;
+            write(1, str, length);
         }
+        return width;
     }
 
     return write(1, str, length);
 }
-
 
 
 
